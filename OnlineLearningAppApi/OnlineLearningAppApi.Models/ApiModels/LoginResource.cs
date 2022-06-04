467c9e10c3bb4db38ac38e2da3bd1629fdb23e22
Adriangrz <adriang1412g@gmail.com>
2022-06-04 22:38:37 +0200

    Change LoginResource properties

--- OnlineLearningAppApi/OnlineLearningAppApi.Models/ApiModels/LoginResource.cs
+++ OnlineLearningAppApi/OnlineLearningAppApi.Models/ApiModels/LoginResource.cs
@@ -11,6 +11,5 @@ namespace OnlineLearningAppApi.Models.ApiModels
         public string Email { get; set; }
         public string Password { get; set; }
         public string ClientId { get; set; }
-        public string RefreshToken { get; set; }
     }
 }
